c25f0672b3db76a86850f6142ee68f3447c6433c
chingkheingamba-luwangthem <chingkheinganbaluwangthem@gmail.com>
2026-03-28 18:35:37 +0530

    update

--- src/main/java/com/ludoarena/service/FeedbackService.java
+++ src/main/java/com/ludoarena/service/FeedbackService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
